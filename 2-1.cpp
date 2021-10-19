3f0b58a4a878b7e0144197825559727f50167639
dima260 <91672276+dima260@users.noreply.github.com>
2021-10-19 00:11:46 +0000

    задание 2-1

---
+++ 2-1.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+  double a,b;
+  cout <<"введите значение"<<endl;
+  cout <<"A"<<endl;
+  cin >> a;
+  cout <<"B"<<endl;
+  cin >> b;
+  cout <<"сумма"<<(a+b)<<endl;
+  cout <<"разность"<<(a-b)<<endl;
+  cout <<"среднее"<<((a+b)/2)<<endl;
+}
